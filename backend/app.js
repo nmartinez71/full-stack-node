a4d2105cb859a74b2fa47085628b6d367b1c6e37
nmartinez71 <nmartinez71@ivytech.edu>
2025-02-26 14:10:46 -0500

    comiit this

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,74 @@
-// Backend (Glitch) - server.js
-const path = require("path"); 
+//const cors = require('cors');
 const express = require('express');
 const app = express();
 const router = express.Router()
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 const Song = require("./models/songs")
+const User = require("./models/users")
 
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 app.use(express.json())
 
 // Enable CORS for all routes
-/* var cors = require('cors') */
+//app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// Your existing routes
 app.use("/api", router)
+
+router.post("/user", async(req,res) =>{
+  if(!req.body.username || !req.body.password){
+     res.status(400).json({error: "Missing username or passwword"})
+  }
+  const newUser = await new User({
+     username: req.body.username,
+     password: req.body.password,
+     status: req.body.status
+  })
+  try{
+     await newUser.save()
+     console.log(newUser)
+     res.sendStatus(201) //created
+  }
+  catch(err){
+     res.status(400).send
+  }
+})
+
+//authenticate a user to sign in
+router.post("/auth", async(req,res) =>{
+  if(!req.body.username || !req.body.password){
+     res.status(401).json({error: "Missing username or password"})
+     return
+  }
+  //find the user in the database
+  try{
+     const user = await User.findOne({username: req.body.username})
+     if (!user){
+        res.status(401).json({error: "User not found"})
+    
+     }
+     else{
+        //check the username and password to see if they match
+        if(user.password === req.body.password){
+           //create a token
+           const token = jwt.encode({username: user.username}, secret)
+           res.json({token: token, username: user.username})
+        }
+        else{
+           res.status(401).json({error: "Invalid password"})
+        }
+     }
+  }
+  catch(err){
+     res.status(400).send(err.message)
+  } 
+  })
+
+
 
 //get songs from db - nef
 router.get("/songs", async(req,res) => {
@@ -52,29 +102,3 @@ router.post("/songs", async (req, res) =>{
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
-
-//Old code -NM
-
-/* Song.find(query, function(err, songs){
-    if (err){
-      res.status(400).send(err)
-    }
-    else{
-      res.json(songs)
-    }
-  }) */
-
-//Old reouter.get method to get songs and send them to the frontend - Neftali
-
-/* router.get('/songs', (req, res) => {
-  const songs = [
-    {title: "We Found Love",
-            artist: "Rihanna",
-            popularity: 10,
-            releaseDate: new Date(2011, 9, 22),
-            genre: ["electro house"]},
-    { title: 'Song 1', artist: 'Artist 1', genre: 'Pop' },
-    { title: 'Song 2', artist: 'Artist 2', genre: 'Rock' },
-  ];
-  res.json(songs);
-}); */
--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 // Creates the database connection from MongoDB Atlas to our Backend - Neftali
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sdev_finalpjt:finalpjt2025@final-pjt.fltqk.mongodb.net/?retryWrites=true&w=majority&appName=final-pjt',{useNewURLParser: true})
+mongoose.connect('mongodb+srv://nmartinez71:nmartinez71@final-pjt.fltqk.mongodb.net/?retryWrites=true&w=majority&appName=final-pjt')
 
 module.exports = mongoose
